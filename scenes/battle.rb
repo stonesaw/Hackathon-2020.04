b11519c35820f27b39d6bf0e4292cac35775e0d9
stonesaw <ishichan1260@gmail.com>
2020-04-16 02:38:24 +0900

    add attack-motion

--- scenes/battle.rb
+++ scenes/battle.rb
@@ -13,11 +13,11 @@ class Enemy
     end
     @hp = data_match('hp')
     @mp = data_match('mp')
+    @max_hp = @hp
+    @max_mp = @mp
     @attack = data_match('attack')
     @block = data_match('block')
     @agility =  data_match('agility')
-    @max_hp = @hp
-    @max_mp = @mp
   end
 
   private
@@ -69,8 +69,8 @@ class Battle
     @@cursor = 0
     @@drawed = []
     @@enemy1 = Enemy.new("slime")
-    @@show_enemy = Sprite.new(7, 1, ["ene", "HP|■■■■■■■■■■|"])
-    @@show_player = Sprite.new(2, 11, ["yu", "HP|■■■■■■■■■■|"])
+    @@show_enemy = Sprite.new(7, 1, ["ene", "HP||"])
+    @@show_player = Sprite.new(2, 11, ["yu", "HP||"])
     @@count = 0
   end
 
@@ -78,26 +78,20 @@ class Battle
     def update
       system("cls")
       Scene.back if Key.down?(Key::ESCAPE)
+      # $playerはinitializeで使えないため
       if @@count == 0
         @@show_enemy.text = self.text_persentage(@@enemy1)
         @@show_player.text = self.text_persentage($player)
-        count = 1
+        @@count = 1
       end
 
       @@drawed = [@@show_enemy, @@show_player]
-      if Key.down?(Key::UP) && (@@cursor % 10) == 1
-        @@cursor -= 1
-      elsif Key.down?(Key::DOWN) && (@@cursor % 10) == 0
-        @@cursor += 1
-      elsif Key.down?(Key::RIGHT) && @@cursor < 10
-        @@cursor += 10
-      elsif Key.down?(Key::LEFT) && @@cursor >= 10
-        @@cursor -= 10
-      end
 
-      self.cursor_update(@@cursor)
+      self.cursor_update
 
       if Key.down?(Key::RETURN)
+        # system("cls")
+        @@drawed << @@msg
         self.enemy_attack if $player.agility < @@enemy1.agility
         if @@cursor == 0
           self.attack
@@ -108,9 +102,7 @@ class Battle
         elsif @@cursor == 11
           self.escape
         end
-        self.enemy_attack if $player.agility >= @@enemy1.agility
-        @@show_enemy.text = self.text_persentage(@@enemy1)
-        @@show_player.text = self.text_persentage($player)
+        self.enemy_attack if $player.agility >= @@enemy1.agility && @@enemy1.hp >= 0
       else
         @@display.draw(@@drawed << [@@com_attack, @@com_greet, @@com_item, @@com_escape])
       end
@@ -118,32 +110,29 @@ class Battle
 
     def draw
       # @@display.draw(@@drawed)
+      # p @@enemy1
+      # p $player
     end
 
-    def attack
-      system("cls")
-      @@drawed << @@msg
-      
-      damage = $player.attack - (@@enemy1.block * rand(0.6..1.0)).round
+    def attack      
+      damage = $player.attack - (@@enemy1.block * rand(0.7..1)).round
       damage = 0 if damage <= 0
       @@enemy1.hp -= damage
 
       damage = " #{damage}" if "#{damage}".length == 1
-      p_enemy_hp = ("#{@@enemy1.hp}".length == 1 ? " " : "") + "#{@@enemy1.hp}"
+      p_enemy_hp = "#{@@enemy1.hp}"
+      p_enemy_hp = (" " + p_enemy_hp) if "#{@@enemy1.hp}".length == 1
 
       @@msg.text = ["#{$player.name}の攻撃！", 
                     "#{@@enemy1.name}に#{damage}のダメージ！"]
-      @@display.draw(@@drawed)
-      sleep(1)
-
       if @@enemy1.hp > 0
-        @@msg.text = "#{@@enemy1.name}のHP残り" + p_enemy_hp
+        @@msg.text += ["#{@@enemy1.name}のHP残り" + p_enemy_hp]
+        self.msg_draw(1)
       else
-        @@msg.text = "#{@@enemy1.name}を倒した"
+        self.defeat
       end
-      system("cls")
-      @@display.draw(@@drawed)
-      sleep(1)
+      @@show_enemy.text = self.text_persentage(@@enemy1)
+      @@show_player.text = self.text_persentage($player)
     end
 
     def greet
@@ -159,9 +148,46 @@ class Battle
       Scene.close
     end
 
+    def msg_draw(time)
+      system("cls")
+      @@display.draw(@@drawed)
+      sleep(time)
+    end
+
     def enemy_attack
-      puts "敵の攻撃"
-      sleep(0.5)
+      @@msg.text = "敵の攻撃"
+      self.msg_draw(1)
+
+      damage = @@enemy1.attack - ($player.block * rand(0.7..1)).round
+      damage = 0 if damage <= 0
+      $player.hp -= damage
+
+      # 文字数調整
+      damage = "#{damage}"
+      ("#{damage}".length % 2).times do |i|
+        damage = " " + damage
+      end
+      p_player_hp = "#{$player.hp}"
+      ("#{$player.hp}".length % 2).times do |i|
+        p_player_hp = " " + p_player_hp
+      end
+
+      @@msg.text = ["#{@@enemy1.name}の攻撃！", 
+                    "#{$player.name}に#{damage}のダメージ！"]
+      @@msg.text += ["#{$player.name}のHP残り" + p_player_hp] if $player.hp > 0
+      
+      @@show_enemy.text = self.text_persentage(@@enemy1)
+      @@show_player.text = self.text_persentage($player)
+      self.msg_draw(1)
+    end
+
+    def defeat
+      @@msg.text = "#{@@enemy1.name}を倒した"
+      self.msg_draw(1)
+      @@msg.text = ["の経験値", "のお金を手に入れた"]
+      self.msg_draw(1)
+      system("cls")
+      Scene.select(1)
     end
 
     def text_persentage(character)
@@ -174,27 +200,44 @@ class Battle
           gauge += per > i ? "■" : "□"
         end
       end
-      print_hp = ("#{character.hp}".length == 1 ? " " : "") + "#{character.hp}"
-      return ["#{character.name}  HP:#{print_hp}/#{character.max_hp}", "HP|#{gauge}|"]
+      print_hp = "#{character.hp}"
+      ("#{character.hp}".length % 2).times do |i|
+        print_hp = " " + print_hp
+      end
+      print_max_hp = "#{character.max_hp}"
+      ("#{character.max_hp}".length % 2).times do |i|
+        print_max_hp = " " + print_max_hp
+      end
+      return ["#{character.name}  HP:#{print_hp}/#{print_max_hp}", "HP|#{gauge}|"]
     end
     
-    def cursor_update(cursor)
-      if cursor == 0
+    def cursor_update
+      if Key.down?(Key::UP) && (@@cursor % 10) == 1
+        @@cursor -= 1
+      elsif Key.down?(Key::DOWN) && (@@cursor % 10) == 0
+        @@cursor += 1
+      elsif Key.down?(Key::RIGHT) && @@cursor < 10
+        @@cursor += 10
+      elsif Key.down?(Key::LEFT) && @@cursor >= 10
+        @@cursor -= 10
+      end
+
+      if @@cursor == 0
         @@com_attack.text = "○  攻撃"
         @@com_greet.text = "    あいさつ"
         @@com_item.text = "    アイテム"
         @@com_escape.text = "    逃げる"
-      elsif cursor == 1
+      elsif @@cursor == 1
         @@com_attack.text = "    攻撃"
         @@com_greet.text = "○  あいさつ"
         @@com_item.text = "    アイテム"
         @@com_escape.text = "    逃げる"
-      elsif cursor == 10
+      elsif @@cursor == 10
         @@com_attack.text = "    攻撃"
         @@com_greet.text = "    あいさつ"
         @@com_item.text = "○  アイテム"
         @@com_escape.text = "    逃げる"
-      elsif cursor == 11
+      elsif @@cursor == 11
         @@com_attack.text = "    攻撃"
         @@com_greet.text = "    あいさつ"
         @@com_item.text = "    アイテム"

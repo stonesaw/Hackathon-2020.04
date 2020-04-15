7ed26e6c3f2157c0bc3838574bf00533c1d7445c
stonesaw <ishichan1260@gmail.com>
2020-04-16 02:37:35 +0900

    add develop key

--- main.rb
+++ main.rb
@@ -34,7 +34,7 @@ class Player
     @max_mp = 60
     @mp = 60
     @attack = 20
-    @block = 20
+    @block = 10
     @agility = 20
   end
 end
@@ -45,6 +45,12 @@ loop do
   Key.update
 
   break if Key.down?(Key::ESCAPE) && Scene.current == 0
+
+  # only-develop-key
+  Scene.next(init: true) if Key.down?(Key::PAGEUP)
+  Scene.back(init: false) if Key.down?(Key::PAGEDOWN)
+  Scene.close if Key.down?(Key::K_END)
+  # ---
 
   Scene.update
   Scene.draw
